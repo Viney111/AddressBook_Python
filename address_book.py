264645816c9054bbaf3f9a4c5a5ff7b586ac9350
Viney <vineykhaneja999@gmail.com>
2022-04-14 08:00:33 +0530

    [Viney] Added: Deleting Contacts by first name

--- address_book.py
+++ address_book.py
@@ -51,19 +51,19 @@ def Storing_contacts_in_list():
         print(ex)
 
 
-def Editing_contacts(con_list):
+def Editing_or_deleting_contacts(con_list):
     """
         Description: Editing Contact Details form Console i.e. by user choice
         Parameters: None
         Returns: Returns a list containing objects of Contact Class that is taken form Console i.e. User
     """
-    edited_person_name = input(
-        "Enter the name of person, whom details you want to edit: ").upper()
+    edited_or_deleted_person_name = input(
+        "Enter the name of person, whom details you want to edit or delete: ").upper()
     try:
         for item in con_list:
-            if item.first_name.upper() == edited_person_name:
+            if item.first_name.upper() == edited_or_deleted_person_name:
                 choice = input(
-                    "Enter choice u want to edit:\n 1 : FN,2 : LN,3 : Address,4 : City,5 : State,6 : ZIP,7 : Phone,8 : Email")
+                    "Enter choice, u want to edit or delete:\n 1 : FN,2 : LN,3 : Address,4 : City,5 : State,6 : ZIP,7 : Phone,8 : Email,9 : For deleting Contact alltogether")
                 if (choice == "1"):
                     fn = input("Enter updated first name: ")
                     item.first_name = fn
@@ -88,8 +88,12 @@ def Editing_contacts(con_list):
                 elif (choice == "8"):
                     email = input("Enter updated email: ")
                     item.email = email
+                elif (choice == "9"):
+                    con_list.remove(item)
                 else:
                     print("Invalid Choice")
+            else:
+                print("The name entered by you does not exist in Contacts list")
     except Exception as ex:
         print(ex)
 
@@ -97,8 +101,9 @@ def Editing_contacts(con_list):
 if __name__ == "__main__":
     contacts_list = []
     Storing_contacts_in_list()
-    user_choice = input("Do u want to edit Contacts \"Y\" OR \"N\":").upper()
+    user_choice = input(
+        "Do u want to edit or delete Contacts \"Y\" OR \"N\":").upper()
     if (user_choice.upper() == "Y"):
-        Editing_contacts(contacts_list)
+        Editing_or_deleting_contacts(contacts_list)
     for item in contacts_list:
         print(str(item))
