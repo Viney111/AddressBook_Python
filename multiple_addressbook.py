a3ad49b7e11fceebd1f2537a88430326b05588cb
Viney <vineykhaneja999@gmail.com>
2022-04-15 16:34:18 +0530

    [Viney] Added: Added City and State Dictionaries

--- multiple_addressbook.py
+++ multiple_addressbook.py
@@ -5,8 +5,12 @@
     @Last Modified time: None
     @Title : Multiple Address Book
 """
-# Importing Address Book
+
+from os import execl
+# Importing Exception class
 from Custom_Exception import Custom_Exception_AddressBook
+
+# Importing Address Book
 import address_book
 
 # Importing Contacts Class
@@ -38,14 +42,41 @@ def adding_contactslist_in_dict(addressbook_dict):
 
 
 def search_person_in_a_city_state(searching_dict):
-    entered_city_or_state = input(
-        "Please enter a city or state for searching a person: ")
-    for key, value in searching_dict.items():
-        for item in value:
-            if (item.city == entered_city_or_state or item.state == entered_city_or_state):
-                print("The person, you searched for, belongs to ",
-                      entered_city_or_state, " & is from this ", key, " address book: ")
-                print(str(item))
+    try:
+        entered_city_or_state = input(
+            "Please enter a city or state for searching a person: ")
+        for key, value in searching_dict.items():
+            lis1 = list(filter(lambda x: (
+                x.city == entered_city_or_state or x.state == entered_city_or_state), value))
+            if len(lis1) > 0:
+                for item in lis1:
+                    print(str(item))
+            else:
+                print(f"No person from {key} belongs to this city or state")
+    except Exception as ex:
+        print(ex)
+
+
+def dictionary_of_city_and_person(searching_dict):
+    try:
+        city_person_dict = {}
+        for contacts_list in searching_dict.values():
+            list(map(lambda x: city_person_dict.setdefault(
+                x.city, []).append(x), contacts_list))
+        return city_person_dict
+    except Exception as ex:
+        print(ex)
+
+
+def dictionary_of_state_and_person(searching_dict):
+    try:
+        state_person_dict = {}
+        for contacts_list in searching_dict.values():
+            list(map(lambda x: state_person_dict.setdefault(
+                x.state, []).append(x), contacts_list))
+        return state_person_dict
+    except Exception as ex:
+        print(ex)
 
 
 if __name__ == "__main__":
@@ -55,3 +86,14 @@ if __name__ == "__main__":
         print("Address Book Name is: ", key)
         for item in value:
             print(str(item))
+    # search_person_in_a_city_state(address_book_dict)
+    cityperson_dict = dictionary_of_city_and_person(address_book_dict)
+    for key, value in cityperson_dict.items():
+        print("Persons residing in ", key, " city are:")
+        for each_contact in value:
+            print(str(each_contact))
+    stateperson_dict = dictionary_of_state_and_person(address_book_dict)
+    for key, value in stateperson_dict.items():
+        print("Persons residing in ", key, " state are:")
+        for each_contact in value:
+            print(str(each_contact))
